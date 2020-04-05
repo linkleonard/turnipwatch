6b8cc0c5bfb0d4a631fdf8348a5bf87144c88e14
leonwlaw <leon.wlaw@gmail.com>
2020-04-05 16:13:49 -0400

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 
 function App() {
@@ -7,17 +7,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form>
+          <label>Current Turnip Price</label>
+          <input type="number" />
+          <button type="submit">Submit</button>
+        </form>
       </header>
     </div>
   );
