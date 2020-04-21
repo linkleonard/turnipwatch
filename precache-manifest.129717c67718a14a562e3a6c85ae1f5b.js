0815084702d190e4bc96bd1e5cfcd8a8ec03e9af
leonwlaw <leon.wlaw@gmail.com>
2020-04-21 00:18:25 -0400

    Updates

---
+++ precache-manifest.129717c67718a14a562e3a6c85ae1f5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff8b5b5319428b516f92aa14fba56402",
+    "url": "/turnipwatch/index.html"
+  },
+  {
+    "revision": "8e27cfeb4ba9dc749e13",
+    "url": "/turnipwatch/static/css/main.7147aefd.chunk.css"
+  },
+  {
+    "revision": "bb5a04568c28babaafb7",
+    "url": "/turnipwatch/static/js/2.51b828f9.chunk.js"
+  },
+  {
+    "revision": "4464eb154f2bab0d31e275750643f12c",
+    "url": "/turnipwatch/static/js/2.51b828f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e27cfeb4ba9dc749e13",
+    "url": "/turnipwatch/static/js/main.10520c25.chunk.js"
+  },
+  {
+    "revision": "46375f04640bad7a29f6",
+    "url": "/turnipwatch/static/js/runtime-main.d95d1394.js"
+  }
+]);
