0abbc88ecb471649a93c93cc0094a14f4d66764f
leonwlaw <leon.wlaw@gmail.com>
2020-04-22 22:59:26 -0400

    Fix unit tests to work with react-router API

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,12 +1,11 @@
 import React from 'react'
 import { Provider } from 'react-redux'
+import { MemoryRouter } from 'react-router'
 import { createStore } from 'redux'
 import { fireEvent, render } from '@testing-library/react'
 
 import { rootReducer } from "./redux/reducers"
 import App from './App'
-import { renderWithRouter } from 'utils/tests'
-import { create } from 'lodash'
 import { LoadWeeklyPrices } from 'redux/actions'
 import { WeekPriceRecord } from 'models'
 
@@ -14,31 +13,28 @@ import { WeekPriceRecord } from 'models'
 const store = createStore(rootReducer)
 
 test('View current price with no history', async () => {
-  const rendered = render(
+  render(
     <Provider store={store}>
-      <App />
+      <MemoryRouter initialEntries={["/price/me"]}>
+        <App />
+      </MemoryRouter>
     </Provider>
   )
-  const {
-    history: { navigate },
-  } = rendered
-
-  await navigate("/price/me")
 })
 
 test('View current price then edit', async () => {
-  const rendered = renderWithRouter(
+  const rendered = render(
     <Provider store={store}>
-      <App />
+      <MemoryRouter initialEntries={["/price/me"]}>
+        <App />
+      </MemoryRouter>
     </Provider>
   )
   const {
-    history: { navigate },
     getByLabelText,
     getByText,
   } = rendered
 
-  await navigate("/price/me")
   fireEvent.change(getByLabelText("Monday AM"), { target: { value: "35" } })
   fireEvent.click(getByText("Submit"))
 })
@@ -55,14 +51,11 @@ test('View historic prices', async () => {
   const store = createStore(rootReducer)
   store.dispatch(LoadWeeklyPrices([weekPriceRecord]))
 
-  const rendered = renderWithRouter(
+  render(
     <Provider store={store}>
-      <App />
+      <MemoryRouter initialEntries={["/price/me/2020/15"]}>
+        <App />
+      </MemoryRouter>
     </Provider>
   )
-  const {
-    history: { navigate },
-  } = rendered
-
-  await navigate("/price/me/2020/15")
 })
